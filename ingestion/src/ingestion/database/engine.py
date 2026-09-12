aab7568fade58a81f0a0ae0b0f81ebaba5c7dbea
Joaquin M. <joaqu26@proton.me>
2026-09-12 18:11:38 +0800

    change env variables

--- ingestion/src/ingestion/database/engine.py
+++ ingestion/src/ingestion/database/engine.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 load_dotenv()
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ["ASYNCPGDBURL"]
 
 engine = create_async_engine(
     DATABASE_URL,
